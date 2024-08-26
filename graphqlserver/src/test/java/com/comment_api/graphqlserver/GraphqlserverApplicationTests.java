390bc08a3e752c1f3d9564bca79c67e0b0c8ee5b
epicseven-cups <jackylin616@gmail.com>
2024-08-25 21:46:58 -0700

    update init

---
+++ graphqlserver/src/test/java/com/comment_api/graphqlserver/GraphqlserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comment_api.graphqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
